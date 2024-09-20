7d2a751a0ceab7d3f6367848b9343adae0b6e07f
Nemanja Tomic <nemanjatomic476@gmail.com>
2024-09-20 12:57:56 +0200

    reset

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa068f157a26ae7d2154be72b9abf7253f0fa18c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee85442bcd59d7397266e7d39eff4f633a0e5db3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
